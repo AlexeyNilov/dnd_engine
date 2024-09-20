c7c3dcfc538b22d923c595d7d4feea13e37a16d5
alexey.nilov <alexey.nilov@ringcentral.com>
2024-09-20 23:28:14 +0200

    add hp validation

--- model/creature.py
+++ model/creature.py
@@ -1,12 +1,23 @@
 from pydantic import BaseModel
+from pydantic import model_validator
 from pydantic import PositiveInt
 
 
 class Creature(BaseModel):
-    """ TBD
-    """
+    """ Simple creature """
 
     id: str
     name: str
     hp: PositiveInt  # Health points
-    max_hp: PositiveInt  # TODO: make sure hp <= max_hp
+    max_hp: PositiveInt
+
+    @model_validator(mode='after')
+    def check_hp_less_than_max_hp(self):  # TODO: test it
+        if self.hp > self.max_hp:
+            raise ValueError('hp must be less than max_hp')
+        return self
+
+    def __setattr__(self, name, value):
+        super().__setattr__(name, value)
+        if name == 'hp':
+            self.check_hp_less_than_max_hp()
